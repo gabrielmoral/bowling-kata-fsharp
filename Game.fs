b488d4ea76583e502138ad308ed02c08c85af724
Gabriel Moral <gabrielmoralortiz@gmail.com>
2017-11-05 15:17:49 +0000

    Refactor and introduce game functions to support the next iterations

--- Game.fs
+++ Game.fs
@@ -2,9 +2,42 @@ module Game
 
 open System
 
-let score (punctuation:string) =
+
+
+type Roll =
+| Simple
+| Missed
+
+type RollInfo = {
+    Points : int;
+    Type : Roll
+}
+
+let scoreThrow throw =
+    match throw with
+    | '-' -> 0
+    | _ -> (throw.ToString() |> int)
+
+let roll throw =
+    let rollType = match throw with
+                    | '-' -> Missed
+                    | _ -> Simple
+    { Points = scoreThrow throw ; Type = rollType}
+
+let filterSimple = fun roll -> match roll.Type with 
+                                | Simple _ -> Some roll 
+                                | Missed -> None
+
+let rolls punctuation =
     punctuation 
     |> Seq.toList
-    |> Seq.filter Char.IsDigit
-    |> Seq.map (string >> int)
-    |> Seq.sum
+    |> Seq.map roll
+
+let sum (rolls:seq<RollInfo>) = 
+    rolls |> Seq.sumBy (fun t -> t.Points)
+
+let score (punctuation:string) =
+    punctuation 
+    |> rolls
+    |> Seq.choose filterSimple
+    |> sum
