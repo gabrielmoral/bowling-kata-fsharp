ce136ddf5bbf13c25129ccce5f830f41f282f444
Gabriel Moral <gabrielmoralortiz@gmail.com>
2017-11-06 21:32:28 +0000

    Intoduce Spare and frames to model the game, maybe not a good decision, will see

--- Game.fs
+++ Game.fs
@@ -2,42 +2,54 @@ module Game
 
 open System
 
-
-
 type Roll =
 | Simple
 | Missed
+| Spare
 
 type RollInfo = {
     Points : int;
     Type : Roll
 }
 
-let scoreThrow throw =
-    match throw with
-    | '-' -> 0
-    | _ -> (throw.ToString() |> int)
+type Frame = {
+    First : RollInfo;
+    Second : RollInfo;
+}
 
 let roll throw =
-    let rollType = match throw with
-                    | '-' -> Missed
-                    | _ -> Simple
-    { Points = scoreThrow throw ; Type = rollType}
-
-let filterSimple = fun roll -> match roll.Type with 
-                                | Simple _ -> Some roll 
-                                | Missed -> None
+    match throw with
+    | '-' -> { Type = Missed; Points = 0 }
+    | '/' -> { Type = Spare; Points = 0}
+    | _ -> { Type = Simple; Points = throw.ToString() |> int }
 
 let rolls punctuation =
     punctuation 
     |> Seq.toList
     |> Seq.map roll
 
-let sum (rolls:seq<RollInfo>) = 
-    rolls |> Seq.sumBy (fun t -> t.Points)
+let frames rolls =
+    let chunks = Seq.chunkBySize 2 rolls //pairwise??
+    chunks |> Seq.map (fun chunk -> { First = chunk.[0]; Second = chunk.[1]}) |> Seq.toArray
 
-let score (punctuation:string) =
+let findFrame frame frames =
+    // let index = Array.IndexOf(frames, frame)
+    
+    // let mutable acc = 10
+    // for identifier = index + 1 to array.Length do
+    //     let frame' = array.[identifier]
+    10
+
+let count frames=
+    Array.fold (fun state frame -> 
+            match frame.First.Type, frame.Second.Type with
+            | Simple, Missed | Missed, Simple | Simple, Simple | Missed, Missed
+                -> frame.First.Points + frame.Second.Points + state
+            | _, Spare | Spare, _ ->  findFrame frame frames) 
+            0 frames
+
+let score punctuation =
     punctuation 
     |> rolls
-    |> Seq.choose filterSimple
-    |> sum
+    |> frames
+    |> count
